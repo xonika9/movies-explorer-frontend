e6310ea7cb05e911282618bb982aa2691ffd6171
xonika9 <xonika9@gmail.com>
2022-10-26 21:11:55 +0300

    burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,32 @@
 import './Header.css';
+import { useState } from 'react';
 
 import { Link } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  function openNavMenu() {
+    setIsNavOpen(true);
+  }
+
+  function closeNavMenu() {
+    setIsNavOpen(false);
+  }
   return (
     <header className="header">
       <Link to="/" className="logo header__logo" />
-      <div className="header__links">
+      <button className="button header__menu-button" onClick={openNavMenu} />
+      <Navigation isOpened={isNavOpen} handleClose={closeNavMenu} />
+      {/* <div className="header__links">
         <Link to="/signup" className="link header__link">
           Регистрация
         </Link>
         <Link to="/signin" className="link header__link button header__button">
           Войти
         </Link>
-      </div>
+      </div> */}
     </header>
   );
 }
