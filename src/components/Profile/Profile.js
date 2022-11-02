cc625121a7cc6574c97cd42d21f08fc87e356225
xonika9 <xonika9@gmail.com>
2022-11-02 15:58:07 +0300

    add profile editing

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,118 +1,130 @@
 import './Profile.css';
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import Header from '../Header/Header';
 import useForm from '../../utils/useForm';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+import InfoTooltip from '../InfoTooltip/InfoTooltip';
 
-function Profile() {
-  const [currentUser, setCurrentUser] = useState({
-    name: 'Виталий',
-    email: 'pochta@yandex.ru',
-  });
-
-  const [isEdited, setIsEdited] = useState(false);
-
+function Profile({
+  loggedIn,
+  onSignout,
+  onUpdateUser,
+  isInfoTooltipOpen,
+  updateUserError,
+  handleInfoTooltipClose,
+  isFormLoading,
+}) {
+  const currentUser = useContext(CurrentUserContext);
   const initialFormValues = {
-    name: {
-      value: '',
-      error: '',
-      isValid: true,
-    },
-    email: {
-      value: '',
-      error: '',
-      isValid: true,
-    },
+    name: currentUser?.name,
+    email: currentUser?.email,
   };
-
-  const { values, handleChange } = useForm(initialFormValues);
-
-  function handleSubmit(evt) {
-    evt.preventDefault();
-  }
-
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+  const { values, errors, handleChange, isFormValid } =
+    useForm(initialFormValues);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onUpdateUser({ name: values.name, email: values.email });
+  };
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      handleInfoTooltipClose();
+    }
+  };
+  useEffect(() => {
+    setIsSubmitDisabled(
+      !isFormValid ||
+        (values.name === currentUser.name &&
+          values.email === currentUser.email) ||
+        isFormLoading,
+    );
+  }, [currentUser, isFormValid, values, isFormLoading]);
   return (
     <>
-      <Header page={'profile'} loggedIn={true} />
+      <Header page={'profile'} loggedIn={loggedIn} />
+      <InfoTooltip
+        errorMessage={updateUserError}
+        isInfoTooltipOpen={isInfoTooltipOpen}
+        onClose={handleInfoTooltipClose}
+        onOverlay={handleOverlayClick}
+      />
       <section className="profile">
-        <h1 className="profile__title">{`Привет, ${currentUser.name}!`}</h1>
-        {isEdited ? (
-          <form
-            className="profile__form"
-            name="profile-form"
-            onSubmit={handleSubmit}
-            noValidate
-          >
-            <label htmlFor="profile-name" className="profile__label">
-              Имя
-              <input
-                className={`profile__input ${
-                  !values.name.isValid && 'profile__input_invalid'
-                }`}
-                type="text"
-                name="name"
-                placeholder="Введите имя"
-                id="profile-name"
-                required
-                onChange={handleChange}
-                value={values.name.value}
-                autoFocus
-              />
-            </label>
-            <span
-              className={`profile__input-error ${
-                !values.name.isValid && 'profile__input-error_visible'
-              }`}
-            >
-              {values.name.error}
-            </span>
-            <label
-              htmlFor="profile-email"
-              className="profile__label profile__label_no-border"
-            >
-              E-mail
-              <input
-                className={`profile__input ${
-                  !values.email.isValid && 'profile__input_invalid'
-                }`}
-                type="email"
-                name="email"
-                placeholder="Введите email"
-                id="profile-email"
-                required
-                onChange={handleChange}
-                value={values.email.value}
-              />
-            </label>
-            <span
-              className={`profile__input-error ${
-                !values.email.isValid && 'profile__input-error_visible'
-              }`}
-            >
-              {values.email.error}
-            </span>
-          </form>
-        ) : (
-          <div className="profile__info">
-            <div className="profile__info-item">
-              <p className="profile__info-title">Имя</p>
-              <p className="profile__info-value">{currentUser.name}</p>
-            </div>
-            <div className="profile__info-item">
-              <p className="profile__info-title">E-mail</p>
-              <p className="profile__info-value">{currentUser.email}</p>
-            </div>
-          </div>
-        )}
-        <button
-          className="link profile__button profile__button_type_edit"
-          type="button"
-          onClick={() => setIsEdited(true)}
+        <h1 className="profile__title">{`Привет, ${currentUser?.name}!`}</h1>
+        <form
+          className="profile__form"
+          name="profile-form"
+          onSubmit={handleSubmit}
+          noValidate
         >
-          Редактировать
-        </button>
+          <label htmlFor="profile-name" className="profile__label">
+            Имя
+            <input
+              className={`profile__input ${
+                errors.name && 'profile__input_invalid'
+              }`}
+              type="text"
+              name="name"
+              id="profile-name"
+              placeholder="Введите имя"
+              minLength="2"
+              maxLength="30"
+              onChange={handleChange}
+              value={values.name}
+              autoComplete={'off'}
+              disabled={isFormLoading}
+              required
+            />
+          </label>
+          <span
+            className={`profile__input-error ${
+              errors.name && 'profile__input-error_visible'
+            }`}
+          >
+            {errors.name}
+          </span>
+          <label
+            htmlFor="profile-email"
+            className="profile__label profile__label_no-border"
+          >
+            E-mail
+            <input
+              className={`profile__input ${
+                errors.email && 'profile__input_invalid'
+              }`}
+              type="email"
+              name="email"
+              id="profile-email"
+              placeholder="Введите email"
+              onChange={handleChange}
+              value={values.email}
+              autoComplete={'off'}
+              disabled={isFormLoading}
+              required
+            />
+          </label>
+          <span
+            className={`profile__input-error ${
+              errors.email && 'profile__input-error_visible'
+            }`}
+          >
+            {errors.email}
+          </span>
+          <button
+            className={`link profile__button profile__button_type_submit ${
+              isSubmitDisabled && 'profile__button_disabled'
+            }`}
+            type="submit"
+            disabled={isSubmitDisabled}
+            aria-label={'Редактировать'}
+          >
+            {isFormLoading ? 'Сохранение...' : 'Редактировать'}
+          </button>
+        </form>
         <button
           className="link profile__button profile__button_logout"
           type="button"
+          aria-label={'Выйти из аккаунта'}
+          onClick={onSignout}
         >
           Выйти из аккаунта
         </button>
--- src/utils/useForm.js
+++ src/utils/useForm.js
@@ -1,20 +1,93 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import isEmail from 'validator/lib/isEmail';
 
-export default function useForm(inputValues) {
-  const [values, setValues] = useState(inputValues);
-
-  function handleChange(evt) {
-    const {
-      name,
-      value,
-      validity: { valid },
-      validationMessage,
-    } = evt.target;
-
+export default function useForm(initialValues) {
+  const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+  const [isFormValid, setIsFormValid] = useState(false);
+  const handleChange = (e) => {
+    const { name, value, validationMessage } = e.target;
     setValues((prevState) => ({
       ...prevState,
-      [name]: { value, isValid: valid, error: validationMessage },
+      [name]: value,
     }));
-  }
-  return { values, setValues, handleChange };
+    setCustomErrors(name, value, validationMessage);
+    setIsFormValid(e.target.closest('form').checkValidity());
+  };
+  const setCustomErrors = (name, value, validationMessage) => {
+    switch (name) {
+      case 'name':
+        if (value.length === 0) {
+          setErrors((prevState) => ({
+            ...prevState,
+            name: 'Поле Имя должно быть заполнено',
+          }));
+        } else if (value.length < 2) {
+          setErrors((prevState) => ({
+            ...prevState,
+            name: 'Длина поля Имя должна быть не меньше 2 символов',
+          }));
+        } else if (value.length > 30) {
+          setErrors((prevState) => ({
+            ...prevState,
+            name: 'Длина поля Имя должна быть не больше 30 символов',
+          }));
+        } else if (!new RegExp(/[A-Za-zА-Яа-я-\s]+$/).test(value)) {
+          setErrors((prevState) => ({
+            ...prevState,
+            name: 'Поле Имя должно содержать латиницу, кирилицу, пробел или дефис',
+          }));
+        } else {
+          setDefaultErrors(name, validationMessage);
+        }
+        break;
+
+      case 'email':
+        if (value.length === 0) {
+          setErrors((prevState) => ({
+            ...prevState,
+            email: 'Поле E-mail должно быть заполнено',
+          }));
+        } else if (!isEmail(value)) {
+          setErrors((prevState) => ({
+            ...prevState,
+            email: 'Введен некорректный E-mail',
+          }));
+        } else {
+          setDefaultErrors(name, validationMessage);
+        }
+        break;
+
+      case 'password':
+        if (value.length === 0) {
+          setErrors((prevState) => ({
+            ...prevState,
+            password: 'Поле Пароль должно быть заполнено',
+          }));
+        } else {
+          setDefaultErrors(name, validationMessage);
+        }
+        break;
+
+      default:
+        setDefaultErrors(name, validationMessage);
+        break;
+    }
+  };
+  const setDefaultErrors = (name, validationMessage) => {
+    setErrors((prevState) => ({
+      ...prevState,
+      [name]: validationMessage,
+    }));
+  };
+  const resetForm = useCallback(
+    (NewValues = {}, newIsFormValid = false) => {
+      setValues(NewValues);
+      setErrors({});
+      setIsFormValid(newIsFormValid);
+    },
+    [setValues, setIsFormValid],
+  );
+
+  return { values, errors, handleChange, isFormValid, resetForm };
 }
