793fa258d6a142762d34968c2c7c9477c0dd0fee
xonika9 <xonika9@gmail.com>
2022-11-02 16:30:15 +0300

    add login, reg, profile edit, signout, like/delete, load saved movies functions to app

---
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,7 @@
+import { Navigate } from 'react-router-dom';
+
+function ProtectedRoute({ component: Component, ...props }) {
+  return props.loggedIn ? <Component {...props} /> : <Navigate to="/" />;
+}
+
+export default ProtectedRoute;
