ee86ed7269d8f81c11ca06b7cb46fb566a7a670d
xonika9 <xonika9@gmail.com>
2023-04-17 20:10:46 +0300

    fix links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,26 +4,24 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="footer">
-      <p className="footer__heading">
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </p>
+      <p className="footer__heading">xonika9 & BeatFilm</p>
       <div className="footer__container">
         <p className="footer__copyright">&copy; {year}</p>
         <ul className="footer__links">
           <li className="footer__links-item">
             <a
               className="link footer__link"
-              href="https://practicum.yandex.ru"
+              href="https://beatfilmfestival.ru/"
               rel="noreferrer"
               target="_blank"
             >
-              Яндекс.Практикум
+              Beat Film Festival
             </a>
           </li>
           <li className="footer__links-item">
             <a
               className="link footer__link"
-              href="https://github.com"
+              href="https://github.com/xonika9/movies-explorer-frontend"
               rel="noreferrer"
               target="_blank"
             >
